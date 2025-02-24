90bdf7fb41eac8ef2ed9722cd82ba0d3a47c2bac
Binaaa04 <sabrinarahmadini60@gmail.com>
2025-02-24 07:56:20 +0700

    there is a change in the date Mon, Feb 24, 2025  8:14:05 AM

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AboutController extends Controller
+class ArticleController extends Controller
 {
     public function __invoke(){
         return "The Laravel Controller is one of the parts where all web functionality is created. In the Controller, settings are made to access the Model related to the Database and also how to send the data to the View. Various processing is also done in the Controller.";
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', HomeController::class);
+Route::get('/', AboutController::class);
 
 
 
